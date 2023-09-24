2bd1bfb80e8099ac60035a34eac8fe56112a7b60
jusiho <pierreblas123@gmail.com>
2023-09-24 11:26:06 -0500

    nice client

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import UserTable from "./UserTable";
 
-async function fetchOrders() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL_LOCAL}/api/orders`);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function fetchOrders() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_URL_LOCAL}/api/orders`);
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 export default async function Home() {
-  const orders = await fetchOrders();
-  
-  const cant = orders.length;
 
   return (
     <div>
@@ -33,35 +31,7 @@ export default async function Home() {
           />
         </Link>
       </div>
-      <p className="text-lg text-center font-bold m-5">
-        Lista de registrados {cant}
-      </p>
-      <table className="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-800 text-gray-200">
-        <thead>
-          <tr className="text-left border-b border-gray-300">
-            <th className="px-4 py-3">id</th>
-            <th className="px-4 py-3">Firstname</th>
-            <th className="px-4 py-3">Lastname</th>
-            <th className="px-4 py-3">Age</th>
-            <th className="px-4 py-3">Sex</th>
-            <th className="px-4 py-3">Empresa</th>
-            <th className="px-4 py-3">Telefono</th>
-            <th className="px-4 py-3">Interes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order: any) => (
-            <tr key={order.id} className="bg-gray-700 border-b border-gray-600">
-              <td className="px-4 py-3">{order.id}</td>
-              <td className="px-4 py-3">{order.billing.first_name}</td>
-              <td className="px-4 py-3">{order.billing.last_name}</td>
-              {order.meta_data.map((meta: any) => (
-                <td key={meta.id} className="px-4 py-3">{meta.value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </div>
   );
 }
