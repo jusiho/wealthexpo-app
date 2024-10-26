833cecf253a5278694cdb449c0d75254f83cae2c
jusiho <pierreblas123@gmail.com>
2024-10-26 14:14:00 -0500

    feat: Mejorar la presentación de categorías en la página de votación con imágenes de fondo

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -3,10 +3,9 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Image,
   Link,
 } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
 import { headers } from "next/headers";
 
 export default async function Page() {
@@ -24,29 +23,39 @@ export default async function Page() {
   const categorias = res.data;
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-2">
-        Votación de Premios Financieros
-      </h1>
-      <p className="text-center text-gray-600 mb-8">
-        Selecciona la categoría en la que deseas votar
-      </p>
+    <div
+      className=" bg-cover bg-center min-h-screen"
+      style={{ backgroundImage: "url('/vote/WECO-AWARDS-FONDO.jpg')" }}
+    >
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-center mb-2">
+          Votación de Premios Financieros
+        </h1>
+        <p className="text-center text-gray-600 mb-8">
+          Selecciona la categoría en la que deseas votar
+        </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {categorias.map((category: any) => (
-          <Link
-            key={category.id}
-            href={`/vote/${category.id}`}
-            className="w-full"
-          >
-            <Card className="cursor-pointer transition-all w-full">
-              <CardBody>
-                {category.title}
-                {/* <div className="text-sm mt-2">{category.descripcion}</div> */}
-              </CardBody>
-            </Card>
-          </Link>
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {categorias.map((category: any) => (
+            <Link
+              key={category.id}
+              href={`/vote/${category.id}`}
+              className="w-full"
+            >
+              <Card className="cursor-pointer transition-all w-full">
+                <Image
+                  removeWrapper
+                  alt="Card background"
+                  className="z-0 w-full h-full object-cover"
+                  src={category.image}
+                />
+                <CardFooter className="absolute bottom-0">
+                  {category.title}
+                </CardFooter>
+              </Card>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
